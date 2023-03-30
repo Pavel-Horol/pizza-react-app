040689edc08faebd85cdb0dfa4dda446b8838c1e
Pavel-Horol <pashahorolskij4@gmail.com>
2023-03-30 22:27:56 +0300

    10.27

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
 				<Header />
     			<div className="content">
 				<Routes>
-					<Route path='/' element={<Home />}/>
+					<Route path='*' element={<Home />}/>
 					<Route path='/cart' element={<Cart/>}/>
 					<Route path='/pizza/:id' element={<FullPizza/>}/>
-					<Route path='*' element={<NotFound/>}/>
+					<Route path='' element={<NotFound/>}/>
 				</Routes>
     			</div>
   		</div>
